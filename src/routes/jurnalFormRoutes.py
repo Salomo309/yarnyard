04209b83aa32fc4f690680d73500e3299cb91553
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2023-04-14 21:28:19 +0700

    delete: jurnalForm controllers

--- src/routes/jurnalFormRoutes.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-from controllers.jurnalFormController import JurnalFormController
-
-jurnalFormRoutes = Blueprint("jurnalFormRoutes", __name__)
-
-jurnalFormRoutes.route("/", methods=["GET"])(JurnalFormController.getJurnal)
